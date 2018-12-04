3e30a6c80c5d98d3b9d28d6a3c028eed290f16d5
Dmitri Samotessov <dimasamo96@gmail.com>
2018-12-04 09:08:32 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  truth = false
+  i = 0
+  while i < collection.length
+    truth = collection[i] if yield(collection[i])
+    i += 1
+    if truth
+      return truth
+    end
+  end
+nil
 end
