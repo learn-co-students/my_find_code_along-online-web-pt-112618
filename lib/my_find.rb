2f085697581a866d177ba02de87e81228bbbad74
Britny Edwards <bredwards2@gmail.com>
2018-12-08 01:50:29 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,4 @@
 
-
 def my_find(collection)
   i = 0
   while i < collection.length
@@ -7,6 +6,3 @@ def my_find(collection)
     i = i + 1
   end
 end
-
-collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
